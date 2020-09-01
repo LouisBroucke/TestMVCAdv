d68a8f7fe2d0b362becf49d0d8078e6c4c15151e
LouisBroucke <louis.broucke@outlook.com>
2020-09-01 10:40:46 +0200

    Implemented Interfaces in Repo

---
+++ VideoData/Repositories/SQLVideoRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using VideoData.Models;
+
+namespace VideoData.Repositories
+{
+    public class SQLVideoRepository : IFilmRepository, IGenreRepository, IKlantRepository, IVerhuringRepository
+    {
+        private VideoDBContext videoDB;
+
+        public SQLVideoRepository(VideoDBContext context)
+        {
+            videoDB = context;
+        }
+
+        public void Add(Verhuring verhuring)
+        {
+            videoDB.Verhuringen.Add(verhuring);
+        }
+
+        public IEnumerable<Film> GetAllFilms()
+        {
+            return videoDB.Films;
+        }
+
+        public IEnumerable<Genre> GetAllGenres()
+        {
+            return videoDB.Genres;
+        }
+
+        public Film GetFilm(int id)
+        {
+            return videoDB.Films.Find(id);
+        }
+
+        public Genre GetGenre(int id)
+        {
+            return videoDB.Genres.Find(id);
+        }
+
+        public Klant GetKlant(int id)
+        {
+            return videoDB.Klanten.Find(id);
+        }
+    }
+}
